11340d3d377f7aa1b26f917e7b7da033ce4b1b3e
ductlv <aws.tutorial.vn@gmail.com>
2022-12-12 19:57:02 +0700

    feat: cognito implemention

--- lib/resources/apiGateway.ts
+++ lib/resources/apiGateway.ts
@@ -1,16 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import { NestedStack, NestedStackProps } from "aws-cdk-lib";
-import { Construct } from "constructs";
+import { NestedStack, NestedStackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 
 import { env } from '../../env';
+import { AuthorizationType, CfnAuthorizer } from 'aws-cdk-lib/aws-apigateway';
+import { CognitoResource } from './cognito';
 
 export class ApiGatewayResource extends NestedStack {
+    private cognito: CognitoResource;
+
     public api: apigateway.RestApi;
+
+    public cognitoAuthorizer: { authorizerId: string; authorizationType: AuthorizationType };
 
     public constructor(scope: Construct, id: string, props?: NestedStackProps) {
         super(scope, id, props);
-        const stackName = env.STACK_NAME;
+        const stackName = env.isProd ? env.STACK_NAME : `${env.STAGE_NAME}-${env.STACK_NAME}`;
         this.api = new apigateway.RestApi(this, `${stackName}-api`, {
             description: 'video linked api gateway',
             deployOptions: {
@@ -18,18 +24,31 @@ export class ApiGatewayResource extends NestedStack {
             },
             // 👇 enable CORS
             defaultCorsPreflightOptions: {
-                allowHeaders: [
-                    'Content-Type',
-                    'X-Amz-Date',
-                    'Authorization',
-                    'X-Api-Key',
-                ],
+                allowHeaders: ['Content-Type', 'X-Amz-Date', 'Authorization', 'X-Api-Key'],
                 allowMethods: ['OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
                 allowCredentials: true,
                 allowOrigins: ['*'],
             },
         });
+        this.cognito = new CognitoResource(this, 'Cognito', props);
+
         // 👇 create an Output for the API URL
-		new cdk.CfnOutput(this, 'apiUrl', { value: this.api.url });
+        new cdk.CfnOutput(this, 'apiUrl', { value: this.api.url });
+
+        const cognitoAuthorizerName = `${stackName}-cognito-authorizer`;
+        const cognitoCfnAuthorizer = new CfnAuthorizer(this, cognitoAuthorizerName, {
+            restApiId: this.api.restApiId,
+            name: cognitoAuthorizerName,
+            type: 'COGNITO_USER_POOLS',
+            identitySource: 'method.request.header.Authorization',
+            providerArns: [this.cognito.userPool.userPoolArn],
+        });
+
+        new cdk.CfnOutput(this, 'cognitoAuthorizerName', { value: cognitoCfnAuthorizer.name });
+
+        this.cognitoAuthorizer = {
+            authorizerId: cognitoCfnAuthorizer.ref,
+            authorizationType: AuthorizationType.COGNITO,
+        };
     }
-};
+}
--- lib/video-linked-stack.ts
+++ lib/video-linked-stack.ts
@@ -1,28 +1,34 @@
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 import { BaseResources } from './resources';
-import { ApplicationProps } from './interfaces/application';
+import { ApplicationProps, Configuration } from './interfaces/application';
 import { HealthCheck } from './health-check';
+import { env } from '../env';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 class AppResources extends cdk.NestedStack {
-	constructor(scope: Construct, id: string, props: ApplicationProps) {
-		super(scope, id, props);
+    constructor(scope: Construct, id: string, props: ApplicationProps) {
+        super(scope, id, props);
 
-		new HealthCheck(this, 'healthCheck', props);
-	};
+        new HealthCheck(this, 'healthCheck', props);
+    }
 }
 
 export class VideoLinkedStack extends cdk.Stack {
-  	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    	super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-		// init base resource
-		const baseResources = new BaseResources(this, 'baseResources');
-		new AppResources(this, 'appResources', { 
-			baseResources
-		})
-  }
+        const configuration: Configuration = {
+            stackName: env.isProd ? env.STACK_NAME : `${env.STAGE_NAME}-${env.STACK_NAME}`,
+        };
+
+        // init base resource
+        const baseResources = new BaseResources(this, 'baseResources');
+
+        new AppResources(this, 'appResources', {
+            configuration,
+            baseResources,
+        });
+    }
 }
